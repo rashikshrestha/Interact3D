01e6ca205472db6a243a47206da5c47e9f3f378b
rashikshrestha <rashikshrestha01@gmail.com>
2024-12-12 18:58:23 -0500

    euler to quat conversion added

--- interact3d/utils/utils_conversion.py
+++ interact3d/utils/utils_conversion.py
@@ -6,3 +6,10 @@ def euler_to_rotmat(euler_angles):
     rotation = R.from_euler('xyz', np.deg2rad(euler_angles))
     rot_mat = rotation.as_matrix()
     return rot_mat
+
+
+def euler_to_quat(euler_angles):
+    rotation = R.from_euler('xyz', np.deg2rad(euler_angles))
+    quat = rotation.as_quat()
+    return quat
+    
